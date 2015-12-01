93ca3f547593078ff65e161c479bec0950850f4b
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2015-12-01 14:56:52 +0000

    ooops

--- PROJ-2/HTML-PHP/VerPaginaRegistos.php
+++ PROJ-2/HTML-PHP/VerPaginaRegistos.php
@@ -29,8 +29,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 $host="db.ist.utl.pt"; // o MySQL esta disponivel nesta maquina
-$user="ist169720"; // -> substituir pelo nome de utilizador
-$password="gfca6559"; // -> substituir pela password dada pelo mysql_reset
+$user="ist172619"; // -> substituir pelo nome de utilizador
+$password="oefc3659"; // -> substituir pela password dada pelo mysql_reset
 $dbname = $user; // a BD tem nome identico ao utilizador
 
 $connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
