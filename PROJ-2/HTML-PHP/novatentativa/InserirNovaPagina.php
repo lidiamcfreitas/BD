a2e1ba79ccf9b5749482ad54d796ff79fd697d19
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-09 23:02:09 +0000

    insert page try

--- PROJ-2/HTML-PHP/novatentativa/InserirNovaPagina.php
+++ PROJ-2/HTML-PHP/novatentativa/InserirNovaPagina.php
@@ -32,17 +32,53 @@
 		$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 
-		$resultado = $connection->prepare("INSERT INTO pagina (userid, pagecounter, nome, idseq, ativa, ppagecounter) VALUES (:userid, 5179, :nomepagina, 1151988, 1 , NULL)");
-		$resultado->bindParam(":nomepagina", $nomepagina);
-		$resultado->bindParam(":userid", $userid);
+		$sequencia = $connection->prepare("INSERT INTO sequencia (userid, moment) VALUES (:userid, NOW())");
+		$sequencia = $bindParam(":userid", $userid);
+
+		$sql_maxmom  = "SELECT s.contador_sequencia ";
+		$sql_maxmom .= "FROM sequencia s  ";
+		$sql_maxmom .= "WHERE s.userid = :userid ";
+		$sql_maxmom .= "  AND s.moment = all ";
+		$sql_maxmom .= "    ( SELECT max(s2.moment) ";
+		$sql_maxmom .= "     FROM sequencia s2  ";
+		$sql_maxmom .= "     WHERE s2.userid = :userid)";
+
+		// idseq quando o momento e maximo
+		$getmoment = $connection->prepare($sql_maxmom);
+		$getmoment = $bindParam(":userid", $userid);
+		$getmoment->execute();
+
+
+		$sql_maxpc  = "SELECT p.pagecounter ";
+		$sql_maxpc .= "FROM pagina p  ";
+		$sql_maxpc .= "WHERE p.userid = :userid ";
+		$sql_maxpc .= "  AND p.pagecounter = all ";
+		$sql_maxpc .= "    ( SELECT max(p2.pagecounter) ";
+		$sql_maxpc .= "     FROM pagina p2  ";
+		$sql_maxpc .= "     WHERE p2.userid = :userid)";
+
+		// maximo page counter do utilizador
+		$getmaxpc = $connection->prepare($sql_maxpc);
+		$getmaxpc = $bindParam(":userid", $userid);
+		$getmaxpc->execute();
+
+
+		$pagina = $connection->prepare("INSERT INTO pagina (userid, pagecounter, nome, idseq, ativa, ppagecounter) VALUES (:userid, :pagecounter, :nomepagina, :idseq, 1 , NULL)");
+		$pagina->bindParam(":nomepagina", $nomepagina);
+		$pagina->bindParam(":idseq", $pagemoment);
+		$pagina->bindParam(":pagecounter", $maxpc);
+		$pagina->bindParam(":userid", $userid);
 
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		 	$nomepagina = test_input($_POST["nomepagina"]);
 		 	$userid = test_input($_POST["userid"]);
 		}
-
-		$resultado->execute();
+		$pagemoment = $getmoment->fetchColumn();
+		$maxpc = $getmaxpc->fetchColumn() + 1;
+		echo $pagemoment;
+		echo $maxpc;
+		$pagina->execute();
 
 
 	} catch (PDOException $e){
