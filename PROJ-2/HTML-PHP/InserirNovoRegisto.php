5922e763daa67b52c66bb659292ae5bc854e5ee6
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2015-12-09 14:53:32 +0000

    cenas

--- PROJ-2/HTML-PHP/InserirNovoRegisto.php
+++ PROJ-2/HTML-PHP/InserirNovoRegisto.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
 	<title>Sistema de leilões de Recursos Marítimos</title>
 	<meta charset="utf-16">
         <link rel="stylesheet" href="normalize.css">
