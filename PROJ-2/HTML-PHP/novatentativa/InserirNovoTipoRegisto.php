5629e190a3c70b229501c0611ed10c32f0cf608e
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2015-12-10 02:33:03 +0000

    sequenciaa

--- PROJ-2/HTML-PHP/novatentativa/InserirNovoTipoRegisto.php
+++ PROJ-2/HTML-PHP/novatentativa/InserirNovoTipoRegisto.php
@@ -33,7 +33,7 @@
 			echo 'userid:'.$uid;
 			echo 'nreg:'.$nreg;
 
-		$sequencia = $connection->prepare("INSERT INTO sequencia (userid) VALUES ($uid)");
+		$sequencia = $connection->prepare("INSERT INTO sequencia (moment, userid) VALUES (current_timestamp, $uid)");
 		$sequencia->execute();
 
 			echo "Acabei de criar uma sequencia";
