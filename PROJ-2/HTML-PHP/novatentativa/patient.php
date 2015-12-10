552f39cfd7811fba4a33c2960e8d7725111a9bf0
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-10 01:38:41 +0000

    something

--- PROJ-2/HTML-PHP/novatentativa/patient.php
+++ PROJ-2/HTML-PHP/novatentativa/patient.php
@@ -14,97 +14,102 @@
     <div class="container">
       <div class="header">
         <ul class="nav nav-pills pull-right" role="tablist">
-			<li role="presentation" class="active"><a href="patient">Ver Pagina</a></li>
-			<li role="presentation"><a href="transfer"> Outras Queries </a></li>
+            <li role="presentation" class="active"><a href="patient">Ver Pagina</a></li>
+            <li role="presentation"><a href="transfer"> Outras Queries </a></li>
         </ul>
         <h3 class="text-muted">Ver Pagina</h3>
       </div>
-	    <div>
-			<form method="post" class="form-inline" action="<?php echo $_SERVER["PHP_SELF"];?>"> 
-				<div class="form-group">
-					<label for="nomepagina">Nome da Página</label>
-					<input type="text" name="nomepagina" placeholder="Nome da Página" required>
-				</div>
-				<div class="form-group">
-					<label for="userid">Identificação do Utilizador</label>
-					<input type="number" name="userid" placeholder="ex: 11321" required>
-				</div>
-				<div class="form-group">
-					<input type="submit" name="submit" class="btn btn-success" value="Show">
-				</div>
-			</form>
-	    </div>
-	    
-	<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST" && ($_POST["nomepagina"] != "" && $_POST["userid"])){
-	
-		$userid = $_POST["userid"];
-		$nomepagina = $_POST["nomepagina"];
-		
-		class TableRows extends RecursiveIteratorIterator { 
-			
-		    function __construct($it) { 
-		        parent::__construct($it, self::LEAVES_ONLY); 
-		    }
+        <div>
+            <form method="post" class="form-inline" action="<?php echo $_SERVER["PHP_SELF"];?>"> 
+                <div class="form-group">
+                    <label for="nomepagina">Nome da Página</label>
+                    <input type="text" name="nomepagina" placeholder="Nome da Página" required>
+                </div>
+                <div class="form-group">
+                    <label for="userid">Identificação do Utilizador</label>
+                    <input type="number" name="userid" placeholder="ex: 11321" required>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="submit" class="btn btn-success" value="Show">
+                </div>
+            </form>
+        </div>
+    <?php
+    
+    if (($_SERVER["REQUEST_METHOD"] == "POST") && ($_POST["userid"] != "") && ($_POST["nomepagina"] != "")){
 
-		    function current() {
-		        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-		    }
+        $userid = $_POST["userid"];
+        $nomepagina = $_POST["nomepagina"];
 
-		    function beginChildren() { 
-		        echo "<tr>"; 
-		    } 
+        class TableRows extends RecursiveIteratorIterator { 
+            
+            function __construct($it) { 
+                parent::__construct($it, self::LEAVES_ONLY); 
+            }
 
-		    function endChildren() { 
-		        echo "</tr>" . "\n";
-		    } 
-		}
+            function current() {
+                return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+            }
 
-		$sql  = "SELECT r.regcounter ";
-		$sql .= "FROM registo AS r  ";
-		$sql .= "WHERE r.ativo = 1 ";
-		$sql .= "  AND r.userid = :userid ";
-		$sql .= "  AND EXISTS  ";
-		$sql .= "    (SELECT rp.pageid,  ";
-		$sql .= "            rp.regid ";
-		$sql .= "     FROM reg_pag AS rp  ";
-		$sql .= "     WHERE r.regcounter = rp.regid ";
-		$sql .= "       AND rp.userid = :userid  ";
-		$sql .= "       AND rp.ativa = 1 ";
-		$sql .= "       AND EXISTS  ";
-		$sql .= "         (SELECT p.pagecounter ";
-		$sql .= "          FROM pagina AS p  ";
-		$sql .= "          WHERE p.pagecounter = rp.pageid ";
-		$sql .= "            AND p.userid = :userid ";
-		$sql .= "            AND p.nome = :nomepagina ";
-		$sql .= "            AND p.ativa = 1))";
+            function beginChildren() { 
+                echo "<tr>"; 
+            } 
 
+            function endChildren() { 
+                echo "</tr>" . "\n";
+            } 
+        }
 
-		$resultado = $connection->prepare($sql);
-		$resultado->bindParam(":nomepagina", $nomepagina);
+        $sql  = "SELECT r.regcounter ";
+        $sql .= "FROM registo AS r  ";
+        $sql .= "WHERE r.ativo = 1 ";
+        $sql .= "  AND r.userid = :userid ";
+        $sql .= "  AND EXISTS  ";
+        $sql .= "    (SELECT rp.pageid,  ";
+        $sql .= "            rp.regid ";
+        $sql .= "     FROM reg_pag AS rp  ";
+        $sql .= "     WHERE r.regcounter = rp.regid ";
+        $sql .= "       AND rp.userid = :userid  ";
+        $sql .= "       AND rp.ativa = 1 ";
+        $sql .= "       AND EXISTS  ";
+        $sql .= "         (SELECT p.pagecounter ";
+        $sql .= "          FROM pagina AS p  ";
+        $sql .= "          WHERE p.pagecounter = rp.pageid ";
+        $sql .= "            AND p.userid = :userid ";
+        $sql .= "            AND p.nome = :nomepagina ";
+        $sql .= "            AND p.ativa = 1))";
+
+        $resultado = $connection->prepare($sql);
+        $resultado->bindParam(":nomepagina", $nomepagina);
                 $resultado->bindParam(":userid", $userid);
-		$resultado->execute();
+        $resultado->execute();
 
-		$result = $resultado->setFetchMode(PDO::FETCH_ASSOC); 
-		$result = $resultado->fetchAll();
-
-
-		echo('<div id="left">');
-		echo('<table class="center"> ');
-		if (empty($result)) {
-			echo "<p>Não existe uma pagina com esse nome";
-		} else {
-			echo "<table style='border: solid 1px black;'>";
-		echo("<tr><th>Registos da Página " . $nomepagina . "</th></tr>\n");
-		    foreach(new TableRows(new RecursiveArrayIterator($result)) as $k=>$v) { 
-		    	echo  $v;
-		    }
-		echo "</table>";
-		}
+        $result = $resultado->setFetchMode(PDO::FETCH_ASSOC); 
+        $result = $resultado->fetchAll();
 
 
-		$con = null;
-	}
+        echo('<div id="left">');
+        echo('<table class="center"> ');
+        if (empty($result)) {
+            echo "<p>Não existe uma pagina com esse nome";
+        } else {
+            echo "<table style='border: solid 1px black;'>";
+        echo("<tr><th>Registos da Página " . $nomepagina . "</th></tr>\n");
+            foreach(new TableRows(new RecursiveArrayIterator($result)) as $k=>$v) { 
+                echo  $v;
+            }
+        echo "</table>";
+        }
+
+    }
+catch (PDOException $e)
+{
+        echo("<p>ERROR: {$e->getMessage()}</p>");
+    }
+
+
+        $con = null;
+    }
     ?>
     </div> <!-- /container -->
   </body>
