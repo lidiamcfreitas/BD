a312149fe3f9aa1e25446d72d308c04b7f7958a6
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-10 12:48:03 +0000

    navbar

--- PROJ-2/HTML-PHP/novatentativa/insertcampo.php
+++ PROJ-2/HTML-PHP/novatentativa/insertcampo.php
@@ -14,10 +14,15 @@
     <div class="container">
       <div class="header">
         <ul class="nav nav-pills pull-right" role="tablist">
-            <li role="presentation"><a href="deletetiporegisto.php">Apagar Tipo</a></li>
+            <li role="presentation class="active""><a href="insertpagina.php"> Inserir Página </a></li>
+            <li role="presentation"><a href="inserttiporegisto.php"> Inserir Tipo </a></li>
+            <li role="presentation"><a href="insertcampo.php"> Inserir Campos </a></li>
+            <li role="presentation"><a href="deletepagina.php"> Apagar Página </a></li>
+            <li role="presentation"><a href="deletetiporegisto.php"> Apagar Tipo </a></li>
+            <li role="presentation"><a href="deletepagina.php"> Apagar Campo de Tipo </a></li>
+            <li role="presentation"><a href="inserttiporegisto.php"> Inserir Valores de Campos</a></li>
             <li role="presentation"><a href="pagina.php"> Ver Pagina </a></li>
-            <li role="presentation class="active""><a href="insertpagina.php"> Inserir Pagina </a></li>
-            <li role="presentation"><a href="deletepagina.php"> Apagar Pagina </a></li>
+
         </ul>
         <h3 class="text-muted">Inserir Pagina</h3>
       </div>
