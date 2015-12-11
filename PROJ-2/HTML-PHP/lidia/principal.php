bab091943537a1a618a30a39487105692ee87d98
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-11 01:43:50 +0000

    .

--- PROJ-2/HTML-PHP/lidia/principal.php
+++ PROJ-2/HTML-PHP/lidia/principal.php
@@ -105,7 +105,7 @@ session_start();
         foreach($resultado_print as $num=>$row){
           echo "<tr>";
           foreach($row as $nome_coluna => $valor_coluna){
-            echo "<td>$valor_coluna</td>";
+            echo "<td><a href='insertpagina.php?pagename=".$valor_coluna."></td>";
           }
           echo "</tr>";
         } ?>
