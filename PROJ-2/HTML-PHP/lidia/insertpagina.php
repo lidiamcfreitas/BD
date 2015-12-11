8e9263d73a27015446f157c05b82cceef05ca3d3
Bruno Cardoso <brunocardoso.lpma@gmail.com>
2015-12-11 05:18:07 +0000

    nbmn

--- PROJ-2/HTML-PHP/lidia/insertpagina.php
+++ PROJ-2/HTML-PHP/lidia/insertpagina.php
@@ -143,15 +143,14 @@ session_start();
     $deu2 = $testarexiste->fetchColumn();
 
     if ($deu2 != 0) {
-      echo "EXISTE ? ENTAO ACTIVA-A";
+      echo "A pagina foi activada";
       //EXISTE ? ENTÃO ACTIVA-A
       $query = "UPDATE pagina SET ativa=1 WHERE nome = :nomepagina";
       $testar =$connection->prepare($query);
       $testar->bindParam(":nomepagina", $nomep);
       $nomep = $nomepagina;
       $testar->execute();
-
-    }else{
+}
         //EXISTE ALGUMA PAGINA COM O MESMO NOME ?
         echo "EXISTE ALGUMA PAGINA MESMO NOME?";
       $teste = "select count(*) from pagina where nome = :nomepagina";
@@ -179,7 +178,7 @@ session_start();
         $maxpc  = $getmaxpcount;
         $pagina->execute();
       }
-    }
+    
 
     }
     $connection = null;
