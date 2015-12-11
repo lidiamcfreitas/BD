87c18478545ca286388640d5371b6b09158c7f8c
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-11 06:05:03 +0000

    .

--- PROJ-2/HTML-PHP/lidia/insertvaloresregisto.php
+++ PROJ-2/HTML-PHP/lidia/insertvaloresregisto.php
@@ -92,12 +92,13 @@ require "connect.php";
         $sql_campos .= "WHERE typecnt = ? and userid = ?";
         $sql_campos .= "  AND ativo = 1;";
         $obj_campos = $connection->prepare($sql_campos);
-        var_dump($obj_campos);
+
 
         $obj_campos->bindParam(array($tipoderegisto1, $userid));
 
         $result_campos = $obj_campos->fetchAll(PDO::FETCH_ASSOC);
-
+        var_dump($result_campos);
+        
         ?>
         <form method="post" class="form-inline" action="<?php echo $_SERVER["PHP_SELF"];?>">
           <tr>
